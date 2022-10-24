10df2511ef2791635a1bdb851415c6cb9ddd01ec
sisaha9 <sisahawork@gmail.com>
2022-10-24 01:47:41 -0500

    Using signal.datatype as well

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from importlib.resources import read_text
 import yaml
 from pathlib import Path
 from dataclasses import dataclass
@@ -9,24 +10,26 @@ from distutils.dir_util import copy_tree
 from distutils.file_util import copy_file
 import re
 
-supported_int_bit_lengths = [8, 16, 32, 64]
+supported_int_bit_lengths = [2, 8, 16, 32, 64]
 supported_float_bit_lengths = [32, 64]
+pkg_name = "raptor_dbw"
 dbc_fp = 'New_Eagle_DBW_3.4.dbc'
 yaml_fp = 'New_Eagle_DBW_3.4.yaml'
-# dbc_fp = 'IAC-CAN1-INDY-V9.dbc'
-# yaml_fp = 'IAC-CAN1-INDY-V9.yaml'
-template_pkg_dir = Path('template_package/')
+template_pkg_dir = Path('dbc_parsing_template/')
 out_pkg_dir = Path('generated') / 'src'
-out_pkg_msg_dir = out_pkg_dir / 'raptor_dbw_msgs'
-out_pkg_can_dir = out_pkg_dir / 'raptor_dbw_can'
+out_pkg_msg_dir = out_pkg_dir / 'pkg_name_msgs'.replace('pkg_name', pkg_name)
+out_pkg_can_dir = out_pkg_dir / 'pkg_name_can'.replace('pkg_name', pkg_name)
 ros_msg_dir = 'msg/'
 out_msg_dir = out_pkg_msg_dir / ros_msg_dir
 out_msg_cmake = out_pkg_msg_dir / 'CMakeLists.txt'
+out_msg_xml = out_pkg_msg_dir / 'package.xml'
 out_dbc_dir = out_pkg_can_dir / 'config'
 out_launch_dir = out_pkg_can_dir / 'launch'
-out_dispatch_file = out_pkg_can_dir / 'include' / 'raptor_dbw_can' / 'dispatch.hpp'
-out_can_header = out_pkg_can_dir / 'include' / 'raptor_dbw_can' / 'raptor_dbw_can.hpp'
-out_can_source = out_pkg_can_dir / 'src' / 'raptor_dbw_can.cpp'
+out_dispatch_file = out_pkg_can_dir / 'include' / 'pkg_name_can'.replace('pkg_name', pkg_name) / 'dispatch.hpp'
+out_can_header = out_pkg_can_dir / 'include' / 'pkg_name_can'.replace('pkg_name', pkg_name) / 'pkg_name_can.hpp'.replace('pkg_name', pkg_name)
+out_can_source = out_pkg_can_dir / 'src' / 'pkg_name_can.cpp'.replace('pkg_name', pkg_name)
+out_can_cmake = out_pkg_can_dir / 'CMakeLists.txt'
+out_can_xml = out_pkg_can_dir / 'package.xml'
 
 @dataclass
 class DbcSignal:
@@ -41,6 +44,15 @@ class DbcMessage:
     id: str
     comm_type: List[str]
     signals: List[DbcSignal]
+
+def replace_package_name(folder_name):
+    for path_object in folder_name.glob('**/*'):
+        if path_object.is_file():
+            path_object_text = path_object.read_text()
+            path_object_text = path_object_text.replace("pkg_name", pkg_name)
+            path_object_text = path_object_text.replace("PKG_NAME", pkg_name.upper())
+            path_object_text = path_object_text.replace("PkgName", pkg_name.replace("_", " ").title().replace(" ",""))
+            path_object.write_text(path_object_text)
 
 def process_interval(bit_lengths, nbits):
     i =  bisect.bisect_right(bit_lengths, nbits)
@@ -64,11 +76,13 @@ def return_bits(signal_length, is_int):
 
 def return_datatype(signal):
     ans = ""
-    if isinstance(signal.scale, float):
+    if signal.is_float or isinstance(signal.scale, float):
         signal_bits = str(return_bits(signal.length, False))
         ans = "float"
     else:
         signal_bits = str(return_bits(signal.length, True))
+        if signal_bits == 2:
+            return "bool"
         if signal.is_signed:
             ans = "int"
         else:
@@ -100,6 +114,8 @@ def create_new_package():
     if Path(out_pkg_dir).is_dir():
         return False
     copy_tree(str(template_pkg_dir), str(out_pkg_dir))
+    for path_object in out_pkg_dir.glob('**/*'):
+        path_object.rename(Path(path_object.parent, path_object.stem.replace("pkg_name", pkg_name) + path_object.suffix))
 
 def make_ros_messages(dbc_msgs, msg_template, out_msg_dir, out_msg_cmake, ros_msg_dir):
     ros_msgs = []
@@ -116,6 +132,8 @@ def make_ros_messages(dbc_msgs, msg_template, out_msg_dir, out_msg_cmake, ros_ms
         ros_msg_names.append(ros_msg_name)
         ros_msg_path = out_msg_dir / ros_msg_name
         ros_msg_path.write_text(ros_msg)
+    xml_out_msg = out_msg_xml.read_text()
+    out_msg_xml.write_text(xml_out_msg)
     cmake_out_msg = out_msg_cmake.read_text()
     cmake_out_msg = cmake_out_msg.replace("MSG_FILES", "\n".join(["\t" + '"' + \
         ros_msg_dir + ros_msg_name + '"' for ros_msg_name in ros_msg_names]).strip())
@@ -144,10 +162,10 @@ def modify_dispatch(dbc_msgs, out_dispatch_file):
     out_dispatch_file.write_text(dispatch_out_text)
 
 def modify_header(dbc_msgs, out_can_header):
-    raptor_msg_imports = ""
-    msg_import_template = '#include "raptor_dbw_msgs/msg/IMPORT.hpp"\n'
-    raptor_usings = ""
-    raptor_usings_template = "using raptor_dbw_msgs::msg::USING;\n"
+    pkg_msg_imports = ""
+    msg_import_template = '#include "pkg_name_msgs/msg/IMPORT.hpp"\n'
+    pkg_usings = ""
+    pkg_usings_template = "using pkg_name_msgs::msg::USING;\n"
     recv_can_msgs = ""
     recv_can_msgs_template = "\tvoid recvCANFUNC(const Frame::SharedPtr msg, DbcMessage * message);\n"
     recv_ros_msgs = ""
@@ -157,26 +175,26 @@ def modify_header(dbc_msgs, out_can_header):
     ros_publishers = ""
     ros_publishers_template = "\trclcpp::Publisher<ROSMSG>::SharedPtr pubROSMSG_;\n"
     for dbc_msg in dbc_msgs:
-        raptor_msg_import_name = dbc_msg.name.lower()
+        pkg_msg_import_name = dbc_msg.name.lower()
         for digit in range(10):
-            raptor_msg_import_name = re.sub(f'_{digit}' , f'{digit}' ,raptor_msg_import_name)
-        raptor_msg_imports += (msg_import_template.replace("IMPORT", raptor_msg_import_name))
-        raptor_usings += (raptor_usings_template.replace("USING", dbc_msg.ros_name))
+            pkg_msg_import_name = re.sub(f'_{digit}' , f'{digit}' ,pkg_msg_import_name)
+        pkg_msg_imports += (msg_import_template.replace("IMPORT", pkg_msg_import_name))
+        pkg_usings += (pkg_usings_template.replace("USING", dbc_msg.ros_name))
         if 'publish' in dbc_msg.comm_type:
             recv_can_msgs += (recv_can_msgs_template.replace("CANFUNC", dbc_msg.ros_name))
         if 'receive' in dbc_msg.comm_type:
             recv_ros_msgs += (recv_ros_msgs_template.replace("ROSFUNC", dbc_msg.ros_name))
         ros_subscribers += (ros_subscribers_template.replace("ROSMSG", dbc_msg.ros_name))
         ros_publishers += (ros_publishers_template.replace("ROSMSG", dbc_msg.ros_name))
-    raptor_msg_imports = raptor_msg_imports.strip()
-    raptor_usings = raptor_usings.strip()
+    pkg_msg_imports = pkg_msg_imports.strip()
+    pkg_usings = pkg_usings.strip()
     recv_can_msgs = recv_can_msgs.strip()
     recv_ros_msgs = recv_ros_msgs.strip()
     ros_subscribers = ros_subscribers.strip()
     ros_publishers = ros_publishers.strip()
     out_can_header_text = out_can_header.read_text()
-    out_can_header_text = out_can_header_text.replace("RAPTOR_MSG_IMPORTS", raptor_msg_imports)
-    out_can_header_text = out_can_header_text.replace("RAPTOR_USING", raptor_usings)
+    out_can_header_text = out_can_header_text.replace("PKG_NAME_MSG_IMPORTS", pkg_msg_imports)
+    out_can_header_text = out_can_header_text.replace("PKG_NAME_USING", pkg_usings)
     out_can_header_text = out_can_header_text.replace("RECV_CAN_MESSAGES", recv_can_msgs)
     out_can_header_text = out_can_header_text.replace("RECV_ROS_MESSAGES", recv_ros_msgs)
     out_can_header_text = out_can_header_text.replace("ROS_SUBSCRIBERS", ros_subscribers)
@@ -187,15 +205,14 @@ def modify_source(dbc_msgs, out_can_source):
     ros_publishers_initialize = ""
     ros_publishers_initialize_template = '\tpubROSMSG_ = this->create_publisher<ROSMSG>("MSGNAME", rclcpp::SensorDataQoS());\n'
     ros_subscribers_initialize = ""
-    ros_subscribers_initialize_template = '\tsubROSMSG_ = this->create_subscription<ROSMSG>("MSGNAME", rclcpp::SensorDataQoS(), std::bind(&RaptorDbwCAN::recvROSMSG, this, std::placeholders::_1));\n'
+    ros_subscribers_initialize_template = '\tsubROSMSG_ = this->create_subscription<ROSMSG>("MSGNAME", rclcpp::SensorDataQoS(), std::bind(&PkgNameCAN::recvROSMSG, this, std::placeholders::_1));\n'
     switch_case_id = ""
     switch_case_recv_id_template = "\t\t\tcase SWITCHID:\n\t\t\t\tRECV_DBC(recvROSMSG);\n\t\t\t\tbreak;\n"
-    switch_case_pub_id_template = "\t\t\tcase SWITCHID:\n\t\t\t\tbreak;\n"
     recv_can_body = ""
-    recv_can_body_template = 'void RaptorDbwCAN::recvROSMSG(const Frame::SharedPtr msg, DbcMessage * message)\n{\n\tROSMSG out;\n\tout.stamp = msg->header.stamp;\n\n\tFILL_SIGNALS\n\n\tpubROSMSG_->publish(out);\n}\n\n'
+    recv_can_body_template = 'void PkgNameCAN::recvROSMSG(const Frame::SharedPtr msg, DbcMessage * message)\n{\n\tROSMSG out;\n\tout.stamp = msg->header.stamp;\n\n\tFILL_SIGNALS\n\n\tpubROSMSG_->publish(out);\n}\n\n'
     recv_can_body_signal_template = '\tout.FIELDNAME = message->GetSignal("SIGNAME")->GetResult();\n'
     recv_ros_body = ""
-    recv_ros_body_template = 'void RaptorDbwCAN::recvROSMSG(const ROSMSG::SharedPtr msg)\n{\n\tNewEagle::DbcMessage * message = dbw_dbc_.GetMessageById(DISPATCH_ID);\n\n\tFILLSIGNALS\n\n\tFrame frame = message->GetFrame();\n\tpub_can_->publish(frame);\n}\n\n'
+    recv_ros_body_template = 'void PkgNameCAN::recvROSMSG(const ROSMSG::SharedPtr msg)\n{\n\tNewEagle::DbcMessage * message = dbc_.GetMessageById(DISPATCH_ID);\n\n\tFILLSIGNALS\n\n\tFrame frame = message->GetFrame();\n\tpub_can_->publish(frame);\n}\n\n'
     recv_ros_body_field_template = '\tmessage->GetSignal("SIGNAME")->SetResult(msg->FIELDNAME);\n'
     for dbc_msg in dbc_msgs:
         if 'publish' in dbc_msg.comm_type:
@@ -208,7 +225,6 @@ def modify_source(dbc_msgs, out_can_source):
             recv_can_body += (recv_can_body_template.replace("ROSMSG", dbc_msg.ros_name).replace("FILL_SIGNALS", recv_can_body_signal))
         if 'receive' in dbc_msg.comm_type:
             ros_subscribers_initialize += (ros_subscribers_initialize_template.replace("ROSMSG", dbc_msg.ros_name).replace("MSGNAME", dbc_msg.name.lower()))
-            # switch_case_id += (switch_case_pub_id_template.replace("SWITCHID", dbc_msg.dispatch_id_name).replace("ROSMSG", dbc_msg.ros_name))
             recv_ros_body_field = ""
             for signal in dbc_msg.signals:
                 recv_ros_body_field += (recv_ros_body_field_template.replace("FIELDNAME", signal.name.lower()).replace("SIGNAME", signal.name))
@@ -243,5 +259,6 @@ def generate_new_package(dbc_fp, yaml_fp, out_msg_dir, out_msg_cmake, out_dbc_di
         modify_dispatch(dbc_msgs, out_dispatch_file)
         modify_header(dbc_msgs, out_can_header)
         modify_source(dbc_msgs, out_can_source)
+        replace_package_name(out_pkg_dir)
 
 generate_new_package(dbc_fp, yaml_fp, out_msg_dir, out_msg_cmake, out_dbc_dir, out_launch_dir, ros_msg_dir, out_can_header, out_can_source)
